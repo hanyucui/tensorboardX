92e2da2cec9d16e1926b6a26525e15bb2c5e7173
Hanyu Cui <hanyu.cui@databricks.com>
2019-10-04 13:35:01 -0700

    DO NOT MERGE, set flag for building pypi packages

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ class PostInstallCommand(install):
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-preparing_PyPI_package = False
+preparing_PyPI_package = True
 version_git = version = '1.8+db1'
 
 if not preparing_PyPI_package:
